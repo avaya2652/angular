4220e33270b8241e5d47bba7e2ec525a70c4afa7
Avaya <avaya.basak@gmail.com>
2020-08-09 01:01:17 +1000

    Event binding, property binding, string manipulation and 2 way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {ServerComponent} from './server/server.component';
@@ -12,7 +13,9 @@ import { ServersComponent } from './servers/servers.component'
     ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
+  public isButtonEnable:boolean = false;
+  public status = "Server not created";
+  public serverName  ='Avaya';
+
+  public onButtonClick = () =>{
+    this.status = 'Server is created'
+  }
+  public onupdateServerName = (event:Event) =>{
+    // this.serverName = event.target.value;
+    console.log(event.target)
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(()=>{
+      this.isButtonEnable = true;
+    },2000)
   }
 
 }
