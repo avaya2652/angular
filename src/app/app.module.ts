1aca2bb2990b2719416ee8b584d1fc913f9029bb
Avaya <avaya.basak@gmail.com>
2020-08-10 00:12:24 +1000

    Assignment 2 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from './server/server.component';
-import { ServersComponent } from './servers/servers.component'
+import { AssignmentComponent } from './assignment/assignment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    AssignmentComponent
   ],
   imports: [
     BrowserModule,
