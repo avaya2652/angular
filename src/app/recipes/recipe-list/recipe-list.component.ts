a3a529cc1732aa9b5401393ff8fcb159d0241a79
Avaya <avaya.basak@gmail.com>
2020-08-25 11:13:11 +1000

    Course project structure completed

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  public recipes: Recipe[] = [
+    new Recipe('Avaya', 'This is desc', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg')
+  ]
+
+  constructor() { 
+    console.log(this.recipes[0].name);
+  }
+
+  ngOnInit(): void {
+  }
+  
+
+}
